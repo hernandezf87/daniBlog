52520b5ad4b444d4e13c20397adda27e5320d1ef
Francisco Hernandez <138fhernandez@gmail.com>
2020-04-22 10:16:31 -0700

    add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_191843) do
+ActiveRecord::Schema.define(version: 2020_04_22_171419) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "descriptions"
+    t.text "descriptionstwo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
