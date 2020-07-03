a5972d17f3d777696e7d1e3bc6b86a6582c10fd4
Francisco Hernandez <138fhernandez@gmail.com>
2020-07-03 14:10:12 -0700

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   #skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @post = Post.all.order("created_at DESC")
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
