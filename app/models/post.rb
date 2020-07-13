0799b66cda9e1d275696e6fee795ce1ec685a906
Francisco Hernandez <138fhernandez@gmail.com>
2020-07-13 06:23:47 -0700

    updated images section

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,16 +6,4 @@ class Post < ApplicationRecord
   def thumbnail input
     return self.images[input].variant(resize: '300x300!').processed
   end
-
-  private
-  def image_type
-    if images.attached? == true
-      errors.add(:images, "are missing")
-    end
-    images.each do |image|
-      if !image.content_type.in?(%('image/jpeg/png'))
-        errors.add(:images, 'needs to be a JPEG or PNG')
-      end
-    end
-  end
 end
